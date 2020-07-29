700fe7f2b62a1e04684da05a1e916732e5deaa85
Varfolomeeva Nataya <natashka-varfolomeeva@mail.ru>
2020-07-29 23:14:57 +0300

    rmErrorPage

--- src/test/java/page/errormessagepage/ErrorMessagePageLocators.java
+++
@@ -1,8 +0,0 @@
-package page.errormessagepage;
-
-import org.openqa.selenium.By;
-
-public class ErrorMessagePageLocators {
-    protected static final By ERROR_LOGIN_MESSAGE = By.xpath("//*[@id=\"alerts-container\"]/div[2]");
-
-}
